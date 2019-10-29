5a59a890d9fffa989471c28f8aed3c8669807531
spencerarrasmith <spencerarrasmith@gmail.com>
2019-10-29 00:56:41 -0600

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+//document.querySelector('html').onclick = function() {
+//    alert('Ouch! Stop poking me!');
+//}
