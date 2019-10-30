d5f546ba168f011901b2c5df77901cb5007ebada
spencerarrasmith <dasoccerguy39@gmail.com>
2019-10-30 09:27:00 +0100

    connecting button to function in scripts file

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,8 @@ myHeading.textContent = 'Hello world!';
 //document.querySelector('html').onclick = function() {
 //    alert('Ouch! Stop poking me!');
 //}
+
+
+function myFunction2() {
+	document.getElementById("demo").innerHTML = "Paragraph changed from this other function";
+}
