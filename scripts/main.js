b07a1017b151eee8267ca4d895e015483f183f82
spencerarrasmith <dasoccerguy39@gmail.com>
2020-04-12 22:27:27 +0100

    testing a javascript function

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,9 @@ function myFunction() {
 function myFunction2() {
 	document.getElementById("demo").innerHTML = "The bottom button did this";
 }
+
+function inputTest() {
+	var input_test1 = document.getElementById("input_test1");
+	var label_test1 = document.getElementById("test1");
+	label_test1.innerHTML = input_test1.value
+}
